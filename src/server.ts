23e57d19c05771b7897a1591e870f781c773d433
Luiz Guilherme de Bem <luizdebem99@gmail.com>
2020-08-04 20:08:19 -0300

    create classes route

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.get(`/`, (req, res) => {
-  return res.status(200).json({ message: 'massa' });
-});
+app.use(routes);
 
 app.listen(3333);
